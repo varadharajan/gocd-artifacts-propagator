ee8cacb3fcf72554d9533e22a189836ad302d9c8
Varadharajan Mukundan <srinathsmn@gmail.com>
2015-11-26 14:47:00 +0100

    Upload to Go Server 15.2 ; Upload test-artifacts

--- uploader/src/main/resources/upload_artifacts.sh
+++ uploader/src/main/resources/upload_artifacts.sh
@@ -6,6 +6,7 @@ set -e
 CURRENT_PIPELINE_LOCATOR=$1 # Example :- http://goserver.com:8153/go/files/foo/1243/UATest/1/UAT
 
 [ -d artifacts ] || mkdir artifacts
+[ -d test_artifacts ] || mkdir test_artifacts
 
 rm -rf tmp_artifacts
 mkdir tmp_artifacts
@@ -18,8 +19,10 @@ if [ -f .artifacts_fetched_from_upstream ]; then
 fi
 
 zip -r artifacts.zip artifacts
+zip -r test_artifacts.zip test_artifacts
 
 echo -e "Uploading artifacts to $CURRENT_PIPELINE_LOCATOR \n"
 curl -u artifacts-propagator:Helpdesk -F zipfile=@artifacts.zip $CURRENT_PIPELINE_LOCATOR
+curl -u artifacts-propagator:Helpdesk -F zipfile=@test_artifacts.zip $CURRENT_PIPELINE_LOCATOR
 echo -e "\n Warming up the artifact cache for downstream pipelines \n"
 curl -u artifacts-propagator:Helpdesk $CURRENT_PIPELINE_LOCATOR/artifacts.zip
